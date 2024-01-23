280a3bd8831a2dbf0f80e433c1a40d10852cbe12
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-01-23 15:31:26 +0100

    posts printed

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,41 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+const containerHtml = document.getElementById("container")
+
+
+posts.forEach((element) => {
+
+    containerHtml.innerHTML += `<div class="post">
+                                    <div class="post__header">
+                                        <div class="post-meta">
+                                            <div class="post-meta__icon">
+                                                <img class="profile-pic" src="${element.author.image}" alt="Phil Mangione">
+                                            </div>
+                                            <div class="post-meta__data">
+                                                <div class="post-meta__author">${element.author.name}</div>
+                                                <div class="post-meta__time">${element.created}</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="post__text">${element.content}</div>
+                                    <div class="post__image">
+                                        <img src="${element.media}" alt="">
+                                    </div>
+                                    <div class="post__footer">
+                                        <div class="likes js-likes">
+                                            <div class="likes__cta">
+                                                <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
+                                                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                                    <span class="like-button__label">Mi Piace</span>
+                                                </a>
+                                            </div>
+                                            <div class="likes__counter">
+                                                Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>`
+});
